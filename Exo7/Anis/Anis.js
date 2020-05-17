d7e948b9896bf6c03d38535ed4c07b4fd310352d
Anis ADLAOUI <63511886+Anis-Adlaoui@users.noreply.github.com>
2020-05-17 15:28:16 +0200

    up

---
+++ Exo7/Anis/Anis.js
@@ -0,0 +1,23 @@
+var cinema[];
+function CreateMovie(Name, IMDB, WATCHED)
+{
+    return {
+        Name: Name,
+        IMDB: IMDB,
+        WATCHED: WATCHED
+    };
+}
+cinema.push(CreateMovie("THE IRISHMAN", 7.9, false)); 
+cinema.push(CreateMovie("THE GOD FATHER", 9.2, false));
+cinema.push(CreateMovie("THE DARK KNIGHT", 9.0, true));
+cinema.push(CreateMovie("THE AVENGERS: END GAME", 8.4, false));
+cinema.push(CreateMovie("TRAINING DAY", 7.7, true));
+cinema.push(CreateMovie("ANGELS AND DEMONS", 6.7, false));
+cinema.push(CreateMovie("BLACK PANTHER", 7.3, false));
+cinema.push(CreateMovie("THE DA VINCI CODE", 6.6, true));
+cinema.push(CreateMovie("INCEPTION", 8.8, true));
+cinema.push(CreateMovie("THE JOKER", 8.5, false));
+
+cinema.forEach(element => {
+    console.log(element);
+});
