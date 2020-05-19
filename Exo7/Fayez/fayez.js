9d4cf96feba16092f9c681bc0a2aa6d167b3edf7
Faiz-ux <63406920+Faiz-ux@users.noreply.github.com>
2020-05-19 14:32:47 +0100

    Update fayez.js

--- Exo7/Fayez/fayez.js
+++ Exo7/Fayez/fayez.js
@@ -1,5 +1,5 @@
 //Home Work
-var movies=[];
+/* var movies=[];
 // Top 10 movies of 2019
 movies[0]= {
     Title : "Parasite",
@@ -60,6 +60,25 @@ movies[9]= {
     Rank : 10,
     watched : false 
 };
+ */
+var movies=[];
+function CreateMovie(Title,Rank,watched) {
+    return{
+        Title: Title,
+        Rank: Rank,
+        watched: watched
+    }
+}
+movies.push(CreateMovie("THE IRISHMAN", 1, false)); 
+movies.push(CreateMovie("THE GOD FATHER", 2, true));
+movies.push(CreateMovie("THE DARK KNIGHT", 3, true));
+movies.push(CreateMovie("THE AVENGERS: END GAME", 4, true));
+movies.push(CreateMovie("TRAINING DAY", 5, true));
+movies.push(CreateMovie("ANGELS AND DEMONS", 6, false));
+movies.push(CreateMovie("BLACK PANTHER", 7, true));
+movies.push(CreateMovie("THE DA VINCI CODE", 8, false));
+movies.push(CreateMovie("INCEPTION", 9, true));
+movies.push(CreateMovie("THE JOKER", 10, true));
 
 movies.forEach(element => {
     console.log("#Rank"+element.Rank+" :");
